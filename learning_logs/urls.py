16501bc0e72bcc7fc95163d3b58a8227c70f3ba6
Hocuda <baam6t@gmail.com>
2024-08-21 02:01:06 +0300

    added forms for creating new topics and entries, and webpages for them

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -10,5 +10,9 @@ urlpatterns = [
 	# Страница, которая отображает все темы
 	path('topics', views.topics, name='topics'),
 	# Страница, посвященная отдельной теме
-	path('topics/<int:topic_id>/', views.topic, name='topic')
+	path('topics/<int:topic_id>/', views.topic, name='topic'),
+	# Страница для добавления новой темы
+	path('new_topic/', views.new_topic, name='new_topic'),
+	# Страница для добавления новой записи
+	path('new_entry/<int:topic_id>/', views.new_entry, name='new_entry'),
 ]
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Topic
+from .forms import TopicForm, EntryForm
 
 def index(request):
 	"""Главная страница 'Журнала наблюдений' """
@@ -17,3 +18,38 @@ def topic(request, topic_id):
 	entries = topic.entry_set.order_by('-date_added')
 	context = {'topic': topic, 'entries': entries}
 	return render(request, 'learning_logs/topic.html', context)
+
+def new_topic(request):
+	"""Добавить новую тему"""
+	if request.method != 'POST':
+		# Никаких данных не отправленно; создать пустую форму
+		form = TopicForm()
+	else:
+		# Полученны данные в POST формате; обработать данные
+		form = TopicForm(data=request.POST)
+		if form.is_valid():
+			form.save()
+			return redirect('learning_logs:topics')
+
+	# Показать пустую или недействительную форму
+	context = {'form': form}
+	return render(request, 'learning_logs/new_topic.html', context)
+
+def new_entry(request, topic_id):
+	"""Добавить новую запись в конкретную тему"""
+	topic = Topic.objects.get(id=topic_id)
+	if request.method != 'POST':
+		# Никаких данных не отправленно; создать пустую форму
+		form = EntryForm()
+	else:
+		# Полученны данные в POST формате; обработать данные
+		form = EntryForm(data=request.POST)
+		if form.is_valid():
+			new_entry = form.save(commit=False)
+			new_entry.topic = topic
+			new_entry.save()
+			return redirect('learning_logs:topic', topic_id=topic_id)
+
+	# Показать пустую или недецствительную форму
+	context = {'topic': topic, 'form': form}
+	return render(request, 'learning_logs/new_entry.html', context)
