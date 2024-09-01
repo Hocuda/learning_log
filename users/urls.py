240580276e79fc346f6465b258f0ed77de59f03c
Hocuda <baam6t@gmail.com>
2024-09-01 23:07:27 +0300

    created a registration webpage, changed base.html, registered a 'new_user'

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path, include
 
+from . import views
+
 app_name = 'users'
 urlpatterns = [
 	# Добавить уставные URL auth (аутентификации)
 	path('', include('django.contrib.auth.urls')),
+	# Страница регестрации
+	path('register/', views.register, name='register'),
 ]
